0e0bd32f2d01e5a3e890552c9d96da77ba6a9db1
Pe3Hu <34793005+Pe3Hu@users.noreply.github.com>
2023-01-04 09:07:22 +0500

    update version 0.0043
    1. Interfaces reposition

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -3,10 +3,11 @@ extends Node
 
 onready var ballroom: Node2D = $Ballroom
 onready var interfaces: CanvasLayer = $Interfaces
-onready var bars: HBoxContainer = $Interfaces/ReferenceRect/Bars
+onready var bars: Control = $Interfaces/Bars
 
 func _ready():
 	set_offsets()
+	set_bars()
 
 func set_offsets() -> void:
 	var l = Global.dict.window_size.center.x
@@ -14,12 +15,32 @@ func set_offsets() -> void:
 	l = round(l)
 	ballroom.position.x = l
 	
-	
+	var dancers = {
+		"Mobs": 1,
+		"Champions": 1
+	}
 	var resize = 0.9
 	var border = (1-resize)*l
-	var interface = bars.get_child(0)
-	interfaces.scale = Vector2.ONE*l/interface.rect_size.x*resize
-#	interfaces.get_node("Champions").rect_position = Vector2.ONE*border
-#
-#	interfaces.get_node("Mobs").rect_position.x = Global.dict.window_size.width*2-border
-#	interfaces.get_node("Mobs").rect_position.y = border
+	
+	for team in dancers.keys():
+		var path = "res://scenes/UI/interface/"+team+"Interface.tscn"
+		var scene = load(path)
+		var new_interface = scene.instance()
+		
+		for _i in dancers[team]:
+			new_interface = scene.instance()
+			bars.get_node(team).add_child(new_interface)
+			
+		bars.get_node(team).rect_scale = Vector2.ONE*l/new_interface.rect_size.x*resize
+		
+		match team:
+			"Champions":
+				bars.get_node(team).rect_position = Vector2.ONE*border
+			"Mobs":
+				bars.get_node(team).rect_position.x = Global.dict.window_size.width-border-new_interface.rect_size.x*bars.get_node(team).rect_scale.x
+				bars.get_node(team).rect_position.y = border
+		#bars.get_node(team).rect_position *= bars.get_node(team).rect_scale
+
+
+func set_bars() -> void:
+	pass
